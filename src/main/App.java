50d9d16b5d8edc21918fb460cf89046c94461e0d
MinhQuang425 <auemyulw@gmail.com>
2025-06-20 08:28:53 +0700

    updata

--- src/main/App.java
+++ src/main/App.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package main;
 import model.Menu;
 import model.Bill;
@@ -26,22 +26,5 @@ public class App {
 
         scanner.close();
         bill.printBill();
-=======
-package src.main;
-import model.MonAn;
-
-public class App {
-    public static void main(String[] args) {
-        MonAn monAn = new MonAn("Cơm Tấm", 35000, "Cơm tấm sườn bì chả", "Món chính");
-        System.out.println("Chào mừng đến với thực đơn!");
-        System.out.println("Thông tin món ăn đang chọn:");
-        hienThiMonAn(monAn);
-    }
-    public static void hienThiMonAn(MonAn mon) {
-        System.out.println("Tên: " + mon.getTen());
-        System.out.println("Giá: " + mon.getGia());
-        System.out.println("Mô tả: " + mon.getMoTa());
-        System.out.println("Danh mục: " + mon.getDanhMuc());
->>>>>>> 4772d24974edbd5faf2188df8b315765bd1b0c68
     }
 }
