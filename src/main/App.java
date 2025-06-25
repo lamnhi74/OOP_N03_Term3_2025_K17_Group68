86a8edfae6f81ee64d0525f8bdc8dc616e3678b4
MinhQuang425 <auemyulw@gmail.com>
2025-06-25 23:37:23 +0700

    sualoi

--- src/main/App.java
+++ src/main/App.java
@@ -1,28 +1,36 @@
 package main;
 import model.Menu;
 import model.Bill;
+import model.Dish;
+import model.Order;
+
 import java.util.Scanner;
+import java.util.Collection;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("\nNhap ten khach hang: ");
-        String tenKH = scanner.nextLine();
-        Bill bill = new Bill(tenKH);
-
         Menu menu = new Menu();
+        int len = menu.size();
+
+        for (int i = 0; i < len; i++) {
+            menu.addDish(null);
+        }
+
+        System.out.print("\nNhap ten khach hang: ");
+        String name = scanner.nextLine();
+        Order order = new Order();
+
         String luaChon;
         do {
-            menu.showMenu();
-            System.out.print("Nhap ID mon an ban muon chon (hoac 'x' de thoat): ");
-            String id = scanner.nextLine();
-            if (id.equalsIgnoreCase("x")) break;
-
+            menu.printMenu();
+            order.addDishToOrder(Dish);
             System.out.print("Ban muon tiep tuc chon mon? (y/n): ");
             luaChon = scanner.nextLine();
         } while (luaChon.equalsIgnoreCase("y"));
 
-        scanner.close();
+        Bill bill = new Bill();
         bill.printBill();
+        scanner.close();
     }
 }
--- test/testBanAn.java
+++ test/testBanAn.java
@@ -1,4 +1,4 @@
-package test.java;
+package test;
 
 import main.java.model.BanAn;
 
@@ -7,34 +7,4 @@ public class testBanAn {
     public static void DatBan(double viTri){
         System.out.println("Đặt bàn số: " +viTri);
     }
-    public void capNhatTrangThai(String trangThai){
-        System.out.println("Trạng thái: " +trangThai);
-    }
 }
-ArrayList <Menu> list = new ArrayList<Menu>();
-    public ArrayList<Menu> addDish(Menu dish) {
-        Scanner scanner = new Scanner (System.in);
-        System.out.println("");
-        list.add(dish);
-        return list;
-    }
-
-    public void printMenu() {
-        int len = list.size();
-        for (int i = 0; i < len; i++) {
-            System.out.println("Dish: " + list.get(i).id + " Name: " + list.get(i).ten + " Price: " + list.get(i).gia + " Type: " + list.get(i).danhMuc );
-
-        }
-
-    }
-
-    public ArrayList<Menu> editDish(double gia) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).gia == gia) {
-                System.out.print("true");
-                list.get(i).gia = gia;
-            }
-
-        }
-        return list;
-    }
